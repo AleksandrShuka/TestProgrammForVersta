cca2aa83b8fce5cf81a041844373d524ee2e0455
Alexander Shchuka <shuka.sashaa@gmail.com>
2022-07-18 16:50:41 +0300

    create db handler

---
+++ Repo/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using TestProgrammForVersta.Data;
+
+namespace TestProgrammForVersta.Repo
+{
+    public interface IRepository
+    {
+        IEnumerable<Order> GetAll();
+        Order? Get(long id);
+        void Insert(Order entity);
+        void Delete(Order entity);
+        void Remove(Order entity);
+        void SaveChanges();
+    }
+}
---
+++ Repo/Repository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestProgrammForVersta.Data;
+
+namespace TestProgrammForVersta.Repo
+{
+    public class Repository : IRepository
+    {
+        private readonly OrdersContext _context;
+        private readonly DbSet<Order> _entities;
+
+        public Repository()
+        {
+            _context = new();
+            _entities = _context.Orders;
+        }
+
+        public Repository(OrdersContext context)
+        {
+            _context = context;
+            _entities = _context.Orders;
+        }
+
+        public void Delete(Order entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entities.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public Order? Get(long id)
+        {
+            return _entities.SingleOrDefault(s => s.Id == id);
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            return _entities.AsEnumerable();
+        }
+
+        public void Insert(Order entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entities.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Remove(Order entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entities.Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
